65057bc00d3fbf464dee7e6f4ddb8eb667f13c79
StanislavVasilev <svasilievalee@gmail.com>
2019-11-28 00:49:29 +0300

    Added word "World" in first command

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -3,7 +3,7 @@
  */
 public class MainClass {
   public static void main(String[] args) {
-    System.out.println("Hello");
+    System.out.println("Hello World");
     System.out.print("!");
     String s = "World";
     System.out.printf("\nHello %s", s);
