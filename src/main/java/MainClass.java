7b67b7837f04a4f60fe0fd131f8f97100910b889
StanislavVasilev <svasilievalee@gmail.com>
2019-11-28 00:40:33 +0300

    Added new class with Scanner's use example

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,11 @@
+/**
+ * Created by Stanislav Vasilev on 19.11.17
+ */
 public class MainClass {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Hello");
+    System.out.print("!");
+    String s = "World";
+    System.out.printf("\nHello %s", s);
   }
 }
